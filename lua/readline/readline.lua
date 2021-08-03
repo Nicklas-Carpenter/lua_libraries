d2ca7195f9b9b7088c2e93a17de23febb14d751d
Nicklas Carpenter <carpenter.nicklas@gmail.com>
2021-08-03 14:06:00 -0700

    Code changes to redirect focus to a LuaJIT library

--- lua/readline/readline.lua
+++
@@ -1,4 +0,0 @@
-local libreadline = package.loadlib("./lua_readline.so", "luaopen_libreadline")
-readline = libreadline()
-
-return readline
--- luajit/lua/test.lua
+++
@@ -1,5 +0,0 @@
-lua = require("lua")
-
-L = lua.luaL_newstate()
-
-
